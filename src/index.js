2728c2d874ec3d8311e013316d9db5436c5d0b5a
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-05-08 14:52:18 -0500

    installed redux and create Review file

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
