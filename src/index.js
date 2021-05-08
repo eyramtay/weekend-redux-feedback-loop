5a597ba8568c3326d34ab991b73c0fee72b88a98
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-05-08 15:15:53 -0500

    added reducers in index file

--- src/index.js
+++ src/index.js
@@ -8,5 +8,22 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 
+const surveryReducer = ( state = {}, action ) => {
+    if (action.type === 'sendFeeling') {
+        state = {...state, feeling: action.payload}
+    }
+    if (action.type === 'sendUnderstanding') {
+        state = {...state, understanding: action.payload}
+    }
+    if (action.type === 'sendSupport') {
+        state = {...state, support: action.payload}
+    }
+    if (action.type === 'sendComments') {
+        state = {...state, comments: action.payload}
+    }
+    return state;
+
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
