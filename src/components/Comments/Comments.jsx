c679ac575f754fcc93d1747bace978ee81db0a40
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-05-08 14:22:48 -0500

    created Comments file

---
+++ src/components/Comments/Comments.jsx
@@ -0,0 +1,3 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -35,9 +35,6 @@ function Support() {
         </>
     )
 
-
-    
-
 }
 
 export default Support;
