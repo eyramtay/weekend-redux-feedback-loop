6ccfd0f2cfafb806eed568d1aa7ff3116b8d7132
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-05-08 14:04:23 -0500

    added functionality to Support component

--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -1,3 +1,33 @@
 import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+function Support() {
+
+    const [ support, setSupport ] = useState('');
+
+    const history = useHistory();
+
+    const dispatch = useDispatch();
+
+    function handleSupport(score) {
+    
+    }
+
+    function sendSupport(score) {
+        if ( score > 5 || score < 1 || score === '' ) {
+    
+        alert( 'Please enter a number between 1-5' );
+        setSupport('');
+    } else { 
+        dispatch({ type: 'sendSupport', payload: score });
+        history.push( '/comments' );
+    }
+}
+
+
+    
+
+}
+
+export default Support;
--- src/components/Understanding/Understanding.jsx
+++ src/components/Understanding/Understanding.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 function Understanding() {
     // local state for Understanding
